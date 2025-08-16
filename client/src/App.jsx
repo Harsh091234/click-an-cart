8584ca58754df7778961ed70823f5fca4d523a34
Harsh091234 <hsharma4090@gmai.com>
2025-08-17 02:26:14 +0530

    navbar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,30 @@ import { useThemeStore } from "./store/useThemeStore"
 import {Routes, Route} from "react-router-dom"
 import SettingsPage from './pages/SettingsPage';
 import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import Navbar from './components/Navbar';
 
 const App = () => {
   const {theme} = useThemeStore();
 
   return (
-    <div className='h-screen' data-theme={theme}>
-      <Routes>
+    <div className='h-screen flex flex-col' data-theme={theme}>
+      
+
+    <Navbar />
+
+     
+    
+      <div className='flex-1 overflow-y-auto py-6 px-8 bg-base-200'>
+          <Routes>
             <Route path="/" element={<HomePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/register" element={<SignupPage />} />
+             <Route path="/login" element={<LoginPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
       </Routes>
+      </div>
+      
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    host: "0.0.0.0",   // allow external devices to connect
+    port: 5173
+  },
+  
   plugins: [react(),   tailwindcss()],
 })
