eff4a2da326f7093a8580cb6ee0c22aca0880e09
Harsh091234 <hsharma4090@gmai.com>
2025-09-13 20:18:07 +0530

    .

--- server/src/server.js
+++ server/src/server.js
@@ -28,9 +28,9 @@ app.use(cors({
   credentials: true,                // allow cookies/auth headers if needed
 }))
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello, World!");
+// })
 
 //routes
 app.use("/api/auth", authRoutes);
