8d3e6ce93fdcc8ae596d6f2aae125e92eb9cbe72
Harsh091234 <hsharma4090@gmail.com>
2026-06-27 13:21:46 +0530

    d

--- apps/server/src/controllers/auth.controller.js
+++ apps/server/src/controllers/auth.controller.js
@@ -32,13 +32,13 @@ const setCookies = (res, refreshToken, accessToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production"? "none": "lax",
     maxAge: 1 * 24 * 60 * 60 * 1000,
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production"? "none": "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
